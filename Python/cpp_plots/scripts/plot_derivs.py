Update -- Fixed boundary derivatives issue where input to spline was not in the correct space, then fixed some plotting functions to account for this

Python/cpp_plots/scripts/plot_derivs.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     # read in data
     an_d1x, an_d1y, an_d2x, an_d2y = np.loadtxt("/mnt/c/Users/dillo/Desktop/work/Uni/Year_5/Project/Code/MSci_Project/C++/outputs/analytical_ds_pdf.csv", dtype='float', delimiter=',', unpack=True, skiprows=1, usecols=[2,3,4,5])
     x,y,num_d1x, num_d1y, num_d2x, num_d2y, num_d3x, num_d3y = np.loadtxt("/mnt/c/Users/dillo/Desktop/work/Uni/Year_5/Project/Code/MSci_Project/C++/outputs/num_ds_pdf.csv", dtype='float', delimiter=',', unpack=True, skiprows=1)
-    x,y,an_num_d1x, an_num_d1y, an_num_d2x, an_num_d2y, an_num_d3x, an_num_d3y = np.loadtxt("/mnt/c/Users/dillo/Desktop/work/Uni/Year_5/Project/Code/MSci_Project/C++/outputs/an_ds_pdf.csv", dtype='float', delimiter=',', unpack=True, skiprows=1)
+    # x,y,an_num_d1x, an_num_d1y, an_num_d2x, an_num_d2y, an_num_d3x, an_num_d3y = np.loadtxt("/mnt/c/Users/dillo/Desktop/work/Uni/Year_5/Project/Code/MSci_Project/C++/outputs/an_ds_pdf.csv", dtype='float', delimiter=',', unpack=True, skiprows=1)
     # x,y,zero_d1x, zero_d1y, zero_d2x, zero_d2y, zero_d3x, zero_d3y = np.loadtxt("/mnt/c/Users/dillo/Desktop/work/Uni/Year_5/Project/Code/MSci_Project/C++/outputs/zero_ds_pdf.csv", dtype='float', delimiter=',', unpack=True, skiprows=1)
     
     
@@ -62,12 +62,12 @@ if __name__ == '__main__':
     num_d3x = num_d3x.reshape((sizey,sizex))
     num_d3y = num_d3y.reshape((sizey,sizex))
     
-    an_num_d1x = an_num_d1x.reshape((sizey,sizex))
-    an_num_d1y = an_num_d1y.reshape((sizey,sizex))
-    an_num_d2x = an_num_d2x.reshape((sizey,sizex))
-    an_num_d2y = an_num_d2y.reshape((sizey,sizex))
-    an_num_d3x = an_num_d3x.reshape((sizey,sizex))
-    an_num_d3y = an_num_d3y.reshape((sizey,sizex))
+    # an_num_d1x = an_num_d1x.reshape((sizey,sizex))
+    # an_num_d1y = an_num_d1y.reshape((sizey,sizex))
+    # an_num_d2x = an_num_d2x.reshape((sizey,sizex))
+    # an_num_d2y = an_num_d2y.reshape((sizey,sizex))
+    # an_num_d3x = an_num_d3x.reshape((sizey,sizex))
+    # an_num_d3y = an_num_d3y.reshape((sizey,sizex))
     
     # calculate percentage errors
     ratio1 = (num_d1x/an_d1x - 1)*100
@@ -75,10 +75,10 @@ if __name__ == '__main__':
     ratio3 = (num_d2x/an_d2x - 1)*100
     ratio4 = (num_d2y/an_d2y - 1)*100
     
-    an_ratio1 = (an_num_d1x/an_d1x - 1)*100
-    an_ratio2 = (an_num_d1y/an_d1y - 1)*100
-    an_ratio3 = (an_num_d2x/an_d2x - 1)*100
-    an_ratio4 = (an_num_d2y/an_d2y - 1)*100
+    # an_ratio1 = (an_num_d1x/an_d1x - 1)*100
+    # an_ratio2 = (an_num_d1y/an_d1y - 1)*100
+    # an_ratio3 = (an_num_d2x/an_d2x - 1)*100
+    # an_ratio4 = (an_num_d2y/an_d2y - 1)*100
     
     # Plot data
     title_size = 14
@@ -123,63 +123,63 @@ if __name__ == '__main__':
     
     fig.savefig("../outputs/pdflike/deriv_err.pdf", dpi=800, format="pdf")
     
-    fig = plt.figure(figsize=(12,6))
-    axs = fig.subplots(1,2).flatten()
+    # fig = plt.figure(figsize=(12,6))
+    # axs = fig.subplots(1,2).flatten()
     
-    plot5 = axs[0].pcolormesh(x,y,num_d3x)
-    axs[0].set_title(r"$\partial^3_x z$", fontsize=title_size)
-    axs[0].label_outer()
+    # plot5 = axs[0].pcolormesh(x,y,num_d3x)
+    # axs[0].set_title(r"$\partial^3_x z$", fontsize=title_size)
+    # axs[0].label_outer()
     
-    plot6 = axs[1].pcolormesh(x,y,num_d3y)
-    axs[1].set_title(r"$\partial^3_y z$", fontsize=title_size)
-    axs[1].label_outer()
+    # plot6 = axs[1].pcolormesh(x,y,num_d3y)
+    # axs[1].set_title(r"$\partial^3_y z$", fontsize=title_size)
+    # axs[1].label_outer()
     
-    cb5 = plt.colorbar(plot5)
-    cb6 = plt.colorbar(plot6)
+    # cb5 = plt.colorbar(plot5)
+    # cb6 = plt.colorbar(plot6)
     
-    fig.supxlabel(r'$\log_{10}x$', fontsize=axes_size)
-    fig.supylabel(r'$\log_{10}Q$', x=0.01, fontsize=axes_size) 
-    fig.tight_layout()
+    # fig.supxlabel(r'$\log_{10}x$', fontsize=axes_size)
+    # fig.supylabel(r'$\log_{10}Q$', x=0.01, fontsize=axes_size) 
+    # fig.tight_layout()
     
-    fig.savefig("../outputs/pdflike/third_derivs.pdf", dpi=800, format="pdf")
+    # fig.savefig("../outputs/pdflike/third_derivs.pdf", dpi=800, format="pdf")
     
-    # plot 2x2 errors with actual function at top left
-    fig = plt.figure(figsize=(8,8))
-    axs = fig.subplots(2,2).flatten()
+    # # plot 2x2 errors with actual function at top left
+    # fig = plt.figure(figsize=(8,8))
+    # axs = fig.subplots(2,2).flatten()
     
-    norm,levels,ticks1 = dynamicColorbar(an_ratio1) # Each has seprate colorbar as they model different things
-    plot1 = axs[0].pcolormesh(x,y,an_ratio1, norm=norm, cmap="seismic")
-    axs[0].set_title(r"$\Delta \partial_x z$", fontsize=title_size)
-    axs[0].label_outer()
+    # norm,levels,ticks1 = dynamicColorbar(an_ratio1) # Each has seprate colorbar as they model different things
+    # plot1 = axs[0].pcolormesh(x,y,an_ratio1, norm=norm, cmap="seismic")
+    # axs[0].set_title(r"$\Delta \partial_x z$", fontsize=title_size)
+    # axs[0].label_outer()
     
-    norm,levels,ticks2 = dynamicColorbar(an_ratio2) # Each has seprate colorbar as they model different things
-    plot2 = axs[1].pcolormesh(x,y,an_ratio2, norm=norm, cmap="seismic")
-    axs[1].set_title(r"$\Delta\partial_y z$", fontsize=title_size)
-    axs[1].label_outer()
+    # norm,levels,ticks2 = dynamicColorbar(an_ratio2) # Each has seprate colorbar as they model different things
+    # plot2 = axs[1].pcolormesh(x,y,an_ratio2, norm=norm, cmap="seismic")
+    # axs[1].set_title(r"$\Delta\partial_y z$", fontsize=title_size)
+    # axs[1].label_outer()
     
-    norm,levels,ticks3 = dynamicColorbar(an_ratio3) # Each has seprate colorbar as they model different things
-    plot3 = axs[2].pcolormesh(x,y,an_ratio3, norm=norm, cmap="seismic")
-    axs[2].set_title(r"$\Delta \partial^2_x z$", fontsize=title_size)
-    axs[2].label_outer()
+    # norm,levels,ticks3 = dynamicColorbar(an_ratio3) # Each has seprate colorbar as they model different things
+    # plot3 = axs[2].pcolormesh(x,y,an_ratio3, norm=norm, cmap="seismic")
+    # axs[2].set_title(r"$\Delta \partial^2_x z$", fontsize=title_size)
+    # axs[2].label_outer()
     
-    norm,levels,ticks4 = dynamicColorbar(an_ratio4) # Each has seprate colorbar as they model different things
-    plot4 = axs[3].pcolormesh(x,y,an_ratio4, norm=norm, cmap="seismic")
-    axs[3].set_title(r"$\Delta\partial^2_y z$", fontsize=title_size)
-    axs[3].label_outer()
+    # norm,levels,ticks4 = dynamicColorbar(an_ratio4) # Each has seprate colorbar as they model different things
+    # plot4 = axs[3].pcolormesh(x,y,an_ratio4, norm=norm, cmap="seismic")
+    # axs[3].set_title(r"$\Delta\partial^2_y z$", fontsize=title_size)
+    # axs[3].label_outer()
     
-    cb1 = plt.colorbar(plot1, ax=axs[0], ticks=ticks1)
-    cb2 = plt.colorbar(plot2, ax=axs[1], ticks=ticks2)
-    cb3 = plt.colorbar(plot3, ax=axs[2], ticks=ticks3)
-    cb4 = plt.colorbar(plot4, ax=axs[3], ticks=ticks4)
+    # cb1 = plt.colorbar(plot1, ax=axs[0], ticks=ticks1)
+    # cb2 = plt.colorbar(plot2, ax=axs[1], ticks=ticks2)
+    # cb3 = plt.colorbar(plot3, ax=axs[2], ticks=ticks3)
+    # cb4 = plt.colorbar(plot4, ax=axs[3], ticks=ticks4)
     
-    cb2.set_label(r"Relative Error ($\%$)", rotation=270, labelpad=20, fontsize=cbar_size)
-    cb4.set_label(r"Relative Error ($\%$)", rotation=270, labelpad=20, fontsize=cbar_size)
+    # cb2.set_label(r"Relative Error ($\%$)", rotation=270, labelpad=20, fontsize=cbar_size)
+    # cb4.set_label(r"Relative Error ($\%$)", rotation=270, labelpad=20, fontsize=cbar_size)
     
-    fig.supxlabel(r'$\log_{10}x$', fontsize=axes_size)
-    fig.supylabel(r'$\log_{10}Q$', x=0.01, fontsize=axes_size) 
-    fig.tight_layout()
+    # fig.supxlabel(r'$\log_{10}x$', fontsize=axes_size)
+    # fig.supylabel(r'$\log_{10}Q$', x=0.01, fontsize=axes_size) 
+    # fig.tight_layout()
     
-    fig.savefig("../outputs/pdflike/an_deriv_err.pdf", dpi=800, format="pdf")    
+    # fig.savefig("../outputs/pdflike/an_deriv_err.pdf", dpi=800, format="pdf")    
         
     
     

Python/cpp_plots/scripts/plot_extrapolation.py
@@ -15,39 +15,31 @@ if __name__ == '__main__':
     sizex = 51
     sizey = 51
     
-    an_d1x,an_d1y,an_d2x, an_d2y = np.loadtxt("/mnt/c/Users/dillo/Desktop/work/Uni/Year_5/Project/Code/MSci_Project/C++/outputs/an_boundary_derivs.csv", dtype='float', delimiter=',', unpack=True, skiprows=1, usecols=[2,3,4,5])
     y,d2x_left, d2x_right = np.loadtxt("/mnt/c/Users/dillo/Desktop/work/Uni/Year_5/Project/Code/MSci_Project/C++/outputs/x_boundary_derivs.csv", dtype='float', delimiter=',', unpack=True, skiprows=1)
     x,d2y_top, d2y_bottom = np.loadtxt("/mnt/c/Users/dillo/Desktop/work/Uni/Year_5/Project/Code/MSci_Project/C++/outputs/y_boundary_derivs.csv", dtype='float', delimiter=',', unpack=True, skiprows=1)
     d4d2xd2y = np.loadtxt("/mnt/c/Users/dillo/Desktop/work/Uni/Year_5/Project/Code/MSci_Project/C++/outputs/corner_boundary_derivs.csv", dtype='float', delimiter=',', unpack=True, skiprows=1, usecols=[2])
-    num_d1x, num_d1y, num_d2x, num_d2y = np.loadtxt("/mnt/c/Users/dillo/Desktop/work/Uni/Year_5/Project/Code/MSci_Project/C++/outputs/num_ds_pdf.csv", dtype='float', delimiter=',', unpack=True, skiprows=1, usecols=[2,3,4,5])
     
-    an_d1x = an_d1x.reshape((sizey,sizex))
-    an_d1y = an_d1y.reshape((sizey,sizex))
-    num_d1x = num_d1x.reshape((sizey,sizex))
-    num_d1y = num_d1y.reshape((sizey,sizex))
-    
-    an_d2x = an_d2x.reshape((sizey,sizex))
-    an_d2y = an_d2y.reshape((sizey,sizex))
-    num_d2x = num_d2x.reshape((sizey,sizex))
-    num_d2y = num_d2y.reshape((sizey,sizex))
+    y,an_d2x_left, an_d2x_right = np.loadtxt("/mnt/c/Users/dillo/Desktop/work/Uni/Year_5/Project/Code/MSci_Project/C++/outputs/an_bound_d2x.csv", dtype='float', delimiter=',', unpack=True, skiprows=1)
+    x,an_d2y_top, an_d2y_bottom = np.loadtxt("/mnt/c/Users/dillo/Desktop/work/Uni/Year_5/Project/Code/MSci_Project/C++/outputs/an_bound_d2y.csv", dtype='float', delimiter=',', unpack=True, skiprows=1)
+    an_d4d2xd2y = np.loadtxt("/mnt/c/Users/dillo/Desktop/work/Uni/Year_5/Project/Code/MSci_Project/C++/outputs/an_bound_corners.csv.csv", dtype='float', delimiter=',', unpack=True, skiprows=1, usecols=[2])
     
     # Boundaries
     fig = plt.figure(figsize=(10,10))
     axs = fig.subplots(2,2).flatten()
 
-    plot = axs[0].plot(x, an_d2y[-1][:], marker="x", linestyle='None', label=r"analytic")
+    plot = axs[0].plot(x, an_d2y_top, marker="x", linestyle='None', label=r"analytic")
     axs[0].plot(x, d2y_top,  marker='.', linestyle='None', label=r"numerical extrapolation")
     axs[0].set_title(r"Top boundary",fontsize=title_size)
     axs[0].set_ylabel(r"$\partial^2_y z(x,y)$",fontsize=axes_size)
     axs[0].label_outer()
     
-    plot = axs[2].plot(x, an_d2y[0][:], marker="x", linestyle='None', label=r"analytic")
+    plot = axs[2].plot(x, an_d2y_bottom, marker="x", linestyle='None', label=r"analytic")
     axs[2].plot(x, d2y_bottom,  marker='.', linestyle='None', label=r"numerical extrapolation")
     axs[2].set_title(r"Bottom boundary",fontsize=title_size)
     axs[2].set_ylabel(r"$\partial^2_y z(x,y)$",fontsize=axes_size)
     axs[2].set_xlabel(r"$\log_{10}x$",fontsize=axes_size)
     
-    plot = axs[1].plot(y, an_d2x.T[-1][:], marker="x", linestyle='None', label=r"analytic")
+    plot = axs[1].plot(y, an_d2x_right, marker="x", linestyle='None', label=r"analytic")
     axs[1].plot(y, d2x_right,  marker='.', linestyle='None', label=r"numerical extrapolation")
     axs[1].set_title(r"Right boundary",fontsize=title_size)
     axs[1].set_ylabel(r"$\partial^2_x z(x,y)$",fontsize=axes_size)
@@ -55,7 +47,7 @@ if __name__ == '__main__':
     axs[1].legend()
     
     
-    plot = axs[3].plot(y, an_d2x.T[0][:], marker="x", linestyle='None', label=r"analytic")
+    plot = axs[3].plot(y, an_d2x_left, marker="x", linestyle='None', label=r"analytic")
     axs[3].plot(y, d2x_left,  marker='.', linestyle='None', label=r"numerical extrapolation")
     axs[3].set_title(r"Left boundary",fontsize=title_size)
     axs[3].set_ylabel(r"$\partial^2_x z(x,y)$",fontsize=axes_size)